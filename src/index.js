95fd2f9b2689c94764b3f75dd05d8a5666fccbce
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2026-06-28 13:36:33 +0530

    feat(all): tab switch on button click

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import './style.css'
 import { createHomepage } from './home.js'
+import { createContactPage } from './contact.js'
+import { createMenuPage } from './menu.js'
 
 console.log('Hi')
 
 createHomepage()
+
+const main = document.querySelector('#content')
+
+const nav = document.querySelector('nav')
+nav.addEventListener('click', (e) => {
+  switch (e.target.id) {
+    case 'about-btn':
+      main.replaceChildren()
+      createContactPage()
+      break;
+    case 'home-btn':
+      main.replaceChildren()
+      createHomepage()
+      break;
+    case 'menu-btn':
+      main.replaceChildren()
+      createMenuPage()
+      break;
+    default:
+      break;
+  }
+})
+
